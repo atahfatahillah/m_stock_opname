4c2c36d60ccfb54ab3ce1223c10b0efc6ad3c173
fatahillah <fatahillah.atah@gmail.com>
2024-07-15 12:41:43 +0700

    feat agp and kotlin version update

--- lib/views/scanner_screen.dart
+++ lib/views/scanner_screen.dart
@@ -3,7 +3,7 @@ import 'package:go_router/go_router.dart';
 import 'package:m_stock_opname/routers/router_utils.dart';
 import 'package:m_stock_opname/services/app_db.dart';
 import 'package:m_stock_opname/utils/locator.dart';
-import 'package:mobile_scanner/mobile_scanner.dart';
+// import 'package:mobile_scanner/mobile_scanner.dart';
 
 class ScannerScreen extends StatefulWidget {
   const ScannerScreen({super.key});
@@ -14,44 +14,45 @@ class ScannerScreen extends StatefulWidget {
 
 class _ScannerScreenState extends State<ScannerScreen> {
   final database = locator<AppDB>();
-  late MobileScannerController _controller;
+  // late MobileScannerController _controller;
   @override
   void initState() {
     super.initState();
-    _controller = MobileScannerController(
-      detectionSpeed: DetectionSpeed.normal,
-      facing: CameraFacing.back,
-      torchEnabled: false,
-    );
+    // _controller = MobileScannerController(
+    //   detectionSpeed: DetectionSpeed.normal,
+    //   facing: CameraFacing.back,
+    //   torchEnabled: false,
+    // );
   }
 
   @override
   void dispose() {
     super.dispose();
-    _controller.stop();
+    // _controller.stop();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Mobile Scanner')),
-      body: MobileScanner(
-        controller: _controller,
-        onDetect: (capture) {
-          final List<Barcode> barcodes = capture.barcodes;
-          for (final barcode in barcodes) {
-            final item =
-                database.itemsDao.findItemsByCode(barcode.rawValue.toString());
-            item.first.then((value) {
-              debugPrint(value?.code);
-              _controller.dispose();
-              _controller.stop();
-              context.goNamed(APP_PAGE.itemDetailScreen.routeName,
-                  pathParameters: {'code': value?.code ?? ""});
-            });
-          }
-        },
-      ),
+      body: const Placeholder(),
+      // body: MobileScanner(
+      //   controller: _controller,
+      //   onDetect: (capture) {
+      //     final List<Barcode> barcodes = capture.barcodes;
+      //     for (final barcode in barcodes) {
+      //       final item =
+      //           database.itemsDao.findItemsByCode(barcode.rawValue.toString());
+      //       item.first.then((value) {
+      //         debugPrint(value?.code);
+      //         _controller.dispose();
+      //         _controller.stop();
+      //         context.goNamed(APP_PAGE.itemDetailScreen.routeName,
+      //             pathParameters: {'code': value?.code ?? ""});
+      //       });
+      //     }
+      //   },
+      // ),
     );
   }
 }
